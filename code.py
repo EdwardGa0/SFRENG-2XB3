c4e91b182931b23abf6198a18383b2c8d4d0926e
Yili Liu <yililiu7@gmail.com>
2021-01-20 16:31:21 -0500

    player ensure group size between 2 and 3

--- code.py
+++ code.py
@@ -2,6 +2,8 @@ def are_valid_groups(sn, groups):
     for i in range(len(sn)):
         inGroup = False
         for j in range(len(groups)):
+            if len(groups[j]) != 2 and len(groups[j]) != 3:
+                return False
 	    for k in range(len(groups[j])):
                 if sn[i] == groups[j][k]:
                     inGroup = True
