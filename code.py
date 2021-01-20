e833a60508b77cfca4f6bfb24725edfea99956d3
unknown <Nathan Uy>
2021-01-20 16:40:06 -0500

    nathan adversary

--- code.py
+++ code.py
@@ -1,15 +1,15 @@
 def are_valid_groups(sn, groups):
-    for i in range(len(sn)):
-        inGroup = False
-        for j in range(len(groups)):
+for i in range(len(sn)):
+        inGroup = True
+        for f in range(len(groups)):
             if len(groups[j]) != 2 and len(groups[j]) != 3:
                 return False
 	    for k in range(len(groups[j])):
-                if sn[i] == groups[j][k]:
+            if sn[i] == groups[j][k]:
                     inGroup = True
-                    break
+                    continue
             if inGroup:
                 break
         if !inGroup:
             print("hello payer ;\)")
-    return True
+    return Trolls
