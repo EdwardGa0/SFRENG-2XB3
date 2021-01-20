c6330bd834eee6732df65adc1eb4fb7ae47c913e
Yili Liu <yililiu7@gmail.com>
2021-01-20 16:37:28 -0500

    yili adversary

--- code.py
+++
@@ -1,8 +0,0 @@
-def are_valid_groups(sn, groups):
-for i in range(len(sn)):
-        inGroup = False
-        for f in range(len(groups)):
-        if !inGroup:
-            ahaha
-            yep
-            asldkjf;akjfa;ldfkjakf
