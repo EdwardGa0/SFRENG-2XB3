5ed6efe0ed17d5879ff045c5c6525f87f528f2f4
Yili Liu <yililiu7@gmail.com>
2021-01-20 16:29:01 -0500

    fixed function version 1

--- code.py
+++ code.py
@@ -2,9 +2,11 @@ def are_valid_groups(sn, groups):
     for i in range(len(sn)):
         inGroup = False
         for j in range(len(groups)):
-		for k in range(len(j)):
-            if sn[i] == groups[j][k]:
-                inGroup = True
+	    for k in range(len(groups[j])):
+                if sn[i] == groups[j][k]:
+                    inGroup = True
+                    break
+            if inGroup:
                 break
         if !inGroup:
             return False
