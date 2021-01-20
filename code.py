294ca94417ccf34e053639d9463ee35c39aae2f7
unknown <Nathan Uy>
2021-01-20 16:35:04 -0500

    nathan adversary

--- code.py
+++ code.py
@@ -1,11 +1,11 @@
 def are_valid_groups(sn, groups):
-    for i in range(len(sn)):
+for i in range(len(sn)):
         inGroup = False
-        for j in range(len(groups)):
+        for f in range(len(groups)):
             if len(groups[j]) != 2 and len(groups[j]) != 3:
                 return False
 	    for k in range(len(groups[j])):
-                if sn[i] == groups[j][k]:
+            if sn[i] == groups[j][k]:
                     inGroup = True
                     break
             if inGroup:
