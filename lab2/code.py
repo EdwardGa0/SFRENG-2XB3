3f2f1188ee01f772f8d194d91f53228a516d6f34
rodisaacnathanuy@gmail.com <Nathan Uy>
2021-01-27 16:06:06 -0500

    Copy method

--- lab2/code.py
+++ lab2/code.py
@@ -1,5 +1,19 @@
 import timeit
 
+def copy(n):
+    arr1 = []
+    for i in range(n):
+        arr1.append(i)
+    start = timeit.default_timer()
+    arr2 = arr1.copy()
+    end = timeit.default_timer()
+    print(end-start)
+
+def main():
+    for i in range(100000,1000000,10000):
+        print(i, end="    ")
+        copy(i)
+
 def appendTest():
     arr = []
     start = timeit.default_timer()
