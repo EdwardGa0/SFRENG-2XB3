987a7308c92b41afdea10c0e8cc8ee72a950d296
EdwardGa0 <edwardgaobot@gmail.com>
2021-01-28 15:01:28 -0500

    individual append timings

--- lab2/code.py
+++ lab2/code.py
@@ -2,14 +2,20 @@ import timeit
 
 def appendTest():
     arr = []
-    f = open('./append.txt', 'w')
+    f1 = open('./append.txt', 'w')
+    f2 = open('./append2.txt', 'w')
     time_sum = 0
     for i in range(1000000):
         start = timeit.default_timer()
         arr.append(33)
         end = timeit.default_timer()
         time_sum += end - start
-        f.write(str(i) + ', ' + str(time_sum) + '\n')
+        f1.write(str(i) + ', ' + str(time_sum) + '\n')
+        f2.write(str(i) + ', ' + str(end - start) + '\n')
+    f1.close()
+    f2.close()
+    
+
 
 
 
@@ -26,3 +32,4 @@ def lookupsTest():
         diff = timeit.default_timer() - start
         times.append(diff)
 
+appendTest()
