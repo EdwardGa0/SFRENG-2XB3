6baba18cccb7b8a73ff436aebe9e8b3f78840252
rodisaacnathanuy@gmail.com <Nathan Uy>
2021-02-03 16:28:55 -0500

    inplace sort added

--- lab3/sorts.py
+++ lab3/sorts.py
@@ -1,3 +1,4 @@
+import random
 # dual pivot
 
 # tri pivot
@@ -18,6 +19,37 @@ for i in range(len(a)):
 def multi_pivot_quicksort_copy(L, p):
     if len(L) < 2:
         return L
-    pivots = L[:p]
+    pivot = L[:p]
     subarrays = [p * []]
 
+#inplace quick sort
+def quicksort_inplace(l, lo, hi): #lo starts at 0, hi is len(l)-1
+    if hi <= lo: #base case
+        return 
+
+    pivot = partition(l, lo, hi) #does the sorting. 
+    #returns the right position of the pivot value.
+
+    quicksort_inplace(l, lo, pivot-1) #sort left elements
+    quicksort_inplace(l, pivot+1, hi) #sort right elements
+    
+def partition(l, lo, hi):
+    pivot = l[hi] #pivot is always at the end.
+
+    small = lo - 1 #position where smaller values will be placed
+
+    for element in range(lo, hi): #not including hi (pivot)
+        if l[element] < pivot:
+            small += 1
+            l[small], l[element] = l[element], l[small]
+
+    l[small+1], l[hi] = l[hi], l[small+1] #put pivot on the right spot
+    return small+1
+
+#inplace sort test
+lst = []
+for i in range(30):
+    lst.append(random.randint(0,50))
+print(lst)
+quicksort_inplace(lst, 0, len(lst)-1)
+print(lst)
