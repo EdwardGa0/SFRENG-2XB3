af4d11bba6f6a09360388cbf53847a9034d3238a
Yili Liu <yililiu7@gmail.com>
2021-02-10 15:14:44 -0500

    worst case timing test

--- lab4/code.py
+++ lab4/code.py
@@ -1,1 +1,68 @@
+import timeit
+from lab4 import *
+from sorts import *
 
+def timing_tests():
+    f = open('./lab3/best_sort.txt', 'w')
+    rand_lst = create_random_list(10000)
+    for i in range(100, 10000, 100):
+        res = []
+        start = timeit.default_timer()
+        my_quicksort(rand_lst[:i])
+        end = timeit.default_timer()
+        res.append(end - start)
+
+        start = timeit.default_timer()
+        dual_pivot_quicksort(rand_lst[:i])
+        end = timeit.default_timer()
+        res.append(end - start)
+
+        start = timeit.default_timer()
+        tri_pivot_quicksort(rand_lst[:i])
+        end = timeit.default_timer()
+        res.append(end - start)
+
+        start = timeit.default_timer()
+        quad_pivot_quicksort(rand_lst[:i])
+        end = timeit.default_timer()
+        res.append(end - start)
+
+        f.write(str(i) + ', ' + ','.join(map(str, res)) + '\n')
+    f.close()
+
+def timing_tests_worstcase():
+    f = open('./worst_case.txt', 'w')
+    for i in range(0, 51):
+        avg = []
+        for _ in range(100):
+            lst = create_near_sorted_list(500, i/100)
+            start = timeit.default_timer()
+            mergesort(lst)
+            end = timeit.default_timer()
+            avg.append(end-start)
+        f.write(str(i/100) + ', ' + str(sum(avg)/len(avg)) + '\n')
+    f.close()
+
+def time_individual(i, sort_func):
+    avg = []
+    for _ in range(50):
+        lst = create_near_sorted_list(1000, i/100)
+        start = timeit.default_timer()
+        sort_func(lst)
+        end = timeit.default_timer()
+        avg.append(end-start)
+    return sum(avg)/len(avg)
+
+def timing_tests_worstcase_comparison():
+    f = open('./lab3/worst_case.txt', 'w')
+    for i in range(0, 101, 5):
+        print(i, end=' ')
+        res = []
+        res.append(time_individual(i, tri_pivot_quicksort))
+        res.append(time_individual(i, bubble_sort))
+        res.append(time_individual(i, selection_sort))
+        res.append(time_individual(i, insertion_sort))
+        f.write(str(i/100) + ', ' + ','.join(map(str, res)) + '\n')
+    f.close()
+
+timing_tests_worstcase()
