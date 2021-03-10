00090b702efb0139bebf5de559f3712b17e1a72f
EdwardGa0 <edwardgaobot@gmail.com>
2021-03-10 15:44:37 -0500

    formatting

--- lab7/graphs.py
+++ lab7/graphs.py
@@ -1,6 +1,8 @@
 from collections import deque
 
-#Undirected graph using an adjacency list
+# Undirected graph using an adjacency list
+
+
 class Graph:
 
     def __init__(self, n):
@@ -25,9 +27,11 @@ class Graph:
     def number_of_nodes():
         return len()
 
+
 def DFS2(g, n1, n2):
     vis = [False for _ in range(g.number_of_nodes())]
     return DFS2recur(g, n1, n2, vis, [n1])
+
 
 def DFS2recur(g, n1, n2, vis, l):
     if n1 == n2:
@@ -43,6 +47,7 @@ def DFS2recur(g, n1, n2, vis, l):
                     return o
                 l.pop()
     return o
+
 
 def BFS2(G, node1, node2):
     Q = deque()
@@ -61,6 +66,7 @@ def BFS2(G, node1, node2):
                 Q.append(new_path)
     return []
 
+
 def DFS3_rec(G, node, visited, pred):
     visited.add(node)
     for adj in G.adj[node]:
@@ -68,26 +74,28 @@ def DFS3_rec(G, node, visited, pred):
             pred[adj] = node
             DFS3_rec(G, adj, visited, pred)
 
+
 def DFS3(G, start):
     visited = set()
     pred = {}
     DFS3_rec(G, start, visited, pred)
     return pred
-    
+
+
 def BSF3(G, node):
     path = {}
     Q = deque([node])
-    marked = {node : True}
+    marked = {node: True}
     for nodes in G.adj:
         if nodes != node:
             marked[nodes] = False
     while len(Q) != 0:
-	current_node = Q.popleft()
-	for nodes in G.adj[current_node]:
-	    if not marked[nodes]:
-	        Q.append(nodes)
-		marked[nodes] = True
-		path[nodes] = current_node
+        current_node = Q.popleft()
+        for nodes in G.adj[current_node]:
+            if not marked[nodes]:
+                Q.append(nodes)
+                marked[nodes] = True
+                path[nodes] = current_node
     return path
 
 # g = Graph(6)
