8dd04663ee01bc6b663e384307dea4e047fd99d2
Yili Liu <yililiu7@gmail.com>
2021-03-10 15:25:12 -0500

    dfs2

--- lab7/graphs.py
+++ lab7/graphs.py
@@ -1,5 +1,24 @@
 from collections import deque
 
+def DFS2(g, n1, n2):
+    vis = [False for _ in range(g.number_of_nodes())]
+    return DFS2recur(g, n1, n2, vis, [n1])
+
+def DFS2recur(g, n1, n2, vis, l):
+    if n1 == n2:
+        return l
+    o = []
+    if vis[n1] == False:
+        vis[n1] = True
+        for node in g.adj[n1]:
+            if vis[node] == False:
+                l.append(node)
+                o = DFS2recur(g, node, n2, vis, l)
+                if len(o) > 0:
+                    return o
+                l.pop()
+    return o
+
 #Undirected graph using an adjacency list
 class Graph:
 
@@ -22,5 +41,13 @@ class Graph:
             self.adj[node1].append(node2)
             self.adj[node2].append(node1)
 
-    def number_of_nodes():
-        return len()
+    def number_of_nodes(self):
+        return len(self.adj)
+
+g = Graph(6)
+g.add_edge(0, 1)
+g.add_edge(1, 2)
+g.add_edge(2, 3)
+g.add_edge(3, 4)
+
+print(DFS2(g, 0, 4))
