e1bdd11f9e6ee5eb16176f21e58cfc4e43adc411
rodisaacnathanuy@gmail.com <Nathan Uy>
2021-03-24 14:52:07 -0400

    Added totaldist, createrandom, mystery, and init.

---
+++ lab9/code.py
@@ -0,0 +1,43 @@
+import math
+
+def total_dist(dist):
+    total = 0
+    for key in dist.keys():
+        total += dist[key]
+    return total
+
+def create_random_complete_graph(n,upper):
+    G = DirectedWeightedGraph()
+    for i in range(n):
+        G.add_node(i)
+    for i in range(n):
+        for j in range(n):
+            if i != j:
+                G.add_edge(i,j,random.randint(1,upper))
+    return G
+
+#Assumes G represents its node as integers 0,1,...,(n-1)
+def mystery(G):
+    n = G.number_of_nodes()
+    d = init_d(G)
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if d[i][j] > d[i][k] + d[k][j]: 
+                    d[i][j] = d[i][k] + d[k][j]
+    return d
+
+def init_d(G):
+    n = G.number_of_nodes()
+    d = [[999999 for j in range(n)] for i in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if G.are_connected(i,j):
+                d[i][j] = G.w(i,j)
+        d[i][i] = 0
+    return d
+
+G = create_random_complete_graph(3, 5)
+print(G.weights)
+print(all_pairs_dijkstra(G))
+print(all_pairs_bellman_ford(G))
