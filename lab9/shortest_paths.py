78548e474dee8001f055b5c2aa21fdb7427ebcf7
rodisaacnathanuy@gmail.com <Nathan Uy>
2021-03-24 16:46:24 -0400

    Fixed bellman_ford

--- lab9/shortest_paths.py
+++ lab9/shortest_paths.py
@@ -194,7 +194,7 @@ def all_pairs_bellman_ford(G):
     matrix = []
     nodes = sorted(list(G.adj.keys()))
     for src in nodes:
-       matrix.append(list(dijkstra(G, src).values()))
+       matrix.append(list(bellman_ford(G, src).values()))
     return matrix
 
 
